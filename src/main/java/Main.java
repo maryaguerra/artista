43b81c99083c1f79b0da5c538f7e7478a5e246c2
Marya <169068359+maryaguerra@users.noreply.github.com>
2024-05-13 14:37:56 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    system.out.println("####################");
+    System.out.println("Cantores: Matheus e Kauan");
+    system.out.println("####################")
+      system.out.println("");
+  }
+
+  syatem.out.println("álbum:na praia 2")
+  syatem.out.printl("ano de lançamento:2017");
+
+system.out.println("");
+
+system.out.println("na praia 2 e o álbum da dupla Matheus e Kauan lançado em 10 de março de 2017
