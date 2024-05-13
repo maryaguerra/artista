13b79352b97769f507f65655e4169c5f5244718d
Marya <169068359+maryaguerra@users.noreply.github.com>
2024-05-13 20:46:17 +0000

    marya

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,15 @@
 public class Main {
   public static void main(String[] args) {
-    system.out.println("####################");
+    System.out.println("####################");
     System.out.println("Cantores: Matheus e Kauan");
-    system.out.println("####################")
-      system.out.println("");
+    System.out.println("####################");
+      System.out.println("");
+  System.out.println("álbum:na praia 2");
+      System.out.printl("ano de lançamento:2017");
+
+    System.out.println("");
+
+    System.out.println("Na praia 2 e o álbum da dupla Matheus e Kauan lançado em 10 de março de 2017 o genero das musicas da dupla e sertanejo 'Na praia 2 e o album da dupla matheus e kauan lançado em 10 de marco de 2017 o genero das musicas da dupla e sertanejo");
+    System.out.println("As musicas do album são:A nossa praia, 3:22  Vim pra ficar, 3:27 Antidoto, 3:03 Oitava dose, 3:17 Exclusividade, 3:13 Te assumir pro brasil, 2:43 Do teto ao chão, 2:38 Tinha que ser eu, 2:54 Nessas horas, 3:29")
   }
-
-  syatem.out.println("álbum:na praia 2")
-  syatem.out.printl("ano de lançamento:2017");
-
-system.out.println("");
-
-system.out.println("na praia 2 e o álbum da dupla Matheus e Kauan lançado em 10 de março de 2017
+}
